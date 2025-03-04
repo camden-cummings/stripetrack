rotation bug fixed

roi_selector_gui_dpg/roiinterface.py
@@ -22,6 +22,7 @@ class ROIInterface:
         self.rois = []
         self.selected_polygon = None
         self.selected_polygon_vert = None
+        self.prev = None
         self.drag_polygon = None
         self.frame_width = frame_width
         self.frame_height = frame_height
@@ -29,7 +30,7 @@ class ROIInterface:
         self.allowed_area_min = 0.0
         self.allowed_area_max = frame_width*frame_height
         self.shift = shift
-
+        
     def left_mouse_press_callback(self):
         """When mouse clicked, checks if current mouse position is near to a polygon or a polygon vertex."""
         x, y = get_mouse_pos(self.shift)
@@ -46,20 +47,20 @@ class ROIInterface:
         elif self.selected_polygon is not None:
             centr = Polygon(self.selected_polygon.lines).centroid
 
-            v, future_v = self.find_future_posn((x, y), centr)
-
-            if -1 <= v <= 1:
-                # doing it this way means we need to reintroduce directionality / pos neg
-                theta = math.acos(v)
-                theta = self.introduce_dir(theta, future_v, centr)
-
-                self.rotate(theta)
+            mouse_posn_on_circle = self.find_future_posn((x, y), centr)
+            theta = math.atan2(mouse_posn_on_circle[1]-centr.y, mouse_posn_on_circle[0]-centr.x)
+            
+            if self.prev is not None:
+                self.rotate(self.prev - theta)
+            
+            self.prev = theta
 
     def left_mouse_release_callback(self):
         """Reset all because mouse is no longer down."""
         self.drag_polygon = None
         self.selected_polygon_vert = None
         self.selected_polygon = None
+        self.prev = None
 
     def check_for_selection(self, mouse_pos):
         """Check if mouse down on poly or poly vertex."""
@@ -136,7 +137,7 @@ class ROIInterface:
         """Rotate polygon to angle."""
         poly = self.selected_polygon.lines
         centr = Polygon(poly).centroid
-
+        
         rot_matrix = [[math.cos(angle), -math.sin(angle)],
                       [math.sin(angle), math.cos(angle)]]
         rotated_poly = np.array([(p[0]-centr.x, p[1]-centr.y) for p in poly]).dot(
@@ -209,7 +210,6 @@ class ROIInterface:
 
         Returns
         -------
-        v : current position on the circle of the selected vertex
         future_v : future position on the circle of the selected vertex
 
 
@@ -223,10 +223,7 @@ class ROIInterface:
         future_v = (centr.x + curr_v[0] / curr_mag_v * radius,
                     centr.y + curr_v[1] / curr_mag_v * radius)
 
-        v = (2 * (radius**2) - math.dist(future_v,
-                                         cursor_posn) ** 2) / (2*radius*radius)
-
-        return v, future_v
+        return future_v
 
     def introduce_dir(self, theta: int, future_v: tuple[int, int], centr: tuple[int, int]) -> int:
         """