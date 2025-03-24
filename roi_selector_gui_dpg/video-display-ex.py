f66d53f27821a89e35d5296c87e76630cd6962fc
Camden Cummings <camden.e.cummings@gmail.com>
2025-03-24 10:58:15 -0400

    added vid/img option

--- roi_selector_gui_dpg/video-display-ex.py
+++ roi_selector_gui_dpg/video-display-ex.py
@@ -4,32 +4,67 @@ import numpy as np
 
 from gui import GUI
 
-dpg.create_context()
+import os
 
-fp = "/home/chamomile/Thyme-lab/data/vids/social_and_many_well/"
-ex_fn = fp + "fc2_save_2025-02-06-151155-0000.mp4"
+fp = "/home/chamomile/Thyme-lab/data/vids/social_and_many_well/to-be-processed/march/"
 
-vidcap = cv2.VideoCapture(ex_fn)
+def find_all_videos_for_tracking(path=None, ext="avi"):
+    """Finds all avi files in current working directory, if path given finds all
+    avi files in path that aren't output of the algorithm."""
+    if path is None:
+        path = os.getcwd()
 
-frame_width = int(vidcap.get(3))
-frame_height = int(vidcap.get(4))
+    files_to_read = []
+    for ex in os.walk(path):
+        for filename in ex[2]:
+            if filename[-3:] == ext and not "tracked" in filename and not "checking" in filename \
+            and not "examine" in filename and not "output" in filename:
+                files_to_read.append(ex[0] + "/" + filename)
 
-m = GUI(ex_fn, frame_width, frame_height)
-while dpg.is_dearpygui_running():
-    cont, curr_img = vidcap.read()
+    return files_to_read
+
+all_files = find_all_videos_for_tracking(fp, ext="png")
+for filename in all_files:    
+    dpg.create_context()
+
+    ext = filename.split('.')[-1] 
+    video = False
+    
+    if ext in ["avi", "mp4"]:
+        vidcap = cv2.VideoCapture(filename)
         
-    if not cont:
-        _ = vidcap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        cont, curr_img = vidcap.read()
+        frame_width = int(vidcap.get(3))
+        frame_height = int(vidcap.get(4))
+        
+        video = True
+        
+    elif ext in ["png"]:
+        curr_img = cv2.imread(filename)
+        shape = curr_img.shape
+        
+        if len(shape) == 2:
+            frame_width, frame_height = curr_img.shape
+        elif len(shape) == 3:
+            frame_height, frame_width, _ = curr_img.shape
+        
+    m = GUI(filename, frame_width, frame_height)
     
-    data = np.flip(curr_img, 2)
-    data = data.ravel()
-    data = np.asfarray(data, dtype='f')
-    texture_data = np.true_divide(data, 255.0)
-    
-    dpg.set_value("texture_tag", texture_data)
-    
-    dpg.render_dearpygui_frame()
-    
-dpg.destroy_context()
+    while dpg.is_dearpygui_running():
+        if video:
+            cont, curr_img = vidcap.read()
+                
+            if not cont:
+                _ = vidcap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                cont, curr_img = vidcap.read()
+        
+        data = np.flip(curr_img, 2)
+        data = data.ravel()
+        data = np.asfarray(data, dtype='f')
+        texture_data = np.true_divide(data, 255.0)
+        
+        dpg.set_value("texture_tag", texture_data)
+        
+        dpg.render_dearpygui_frame()
+        
+    dpg.destroy_context()
 
