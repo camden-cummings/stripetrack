fa98b8e43f5a164e2dd8d8d9826d220d8e7cc9c6
Camden Cummings <camden.e.cummings@gmail.com>
2025-05-07 14:02:24 -0400

    comments

--- contour_definer.py
+++ contour_definer.py
@@ -7,8 +7,13 @@ Created on Tue Feb 18 12:02:02 2025
 
 class ContourDefiner:
     def __init__(self):
+        # method used to analyze fish
         self.cv_method = ""
+
+        # threshold used for that method
         self.thresh = 155
+
+        # minimum fish centroid size accepted
         self.centroid_size = 70
         
     def cv_alg_change(self, sender_id, data):
